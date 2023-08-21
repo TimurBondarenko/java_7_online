bf25c139eff6bdc44bbd24eba470fab423e732c2
TimurBondarenko <timurbondarenko404@gmail.com>
2023-08-21 13:39:24 +0300

    Дз 8

--- src/main/java/java7onlineHWs/hw8/Main.java
+++ src/main/java/java7onlineHWs/hw8/Main.java
@@ -4,15 +4,19 @@ public class Main {
 
     public static void main(String[] args) {
         String stc = "Hello World";
-        System.out.println(reverse(stc, 0, 4));
+        System.out.println(reverse(stc, 1, 4));
     }
 
     public static String reverse(String src) {
-        StringBuilder reversed = new StringBuilder();
-        for (int i = src.length() - 1; i >= 0; i--) {
-            reversed.append(src.charAt(i));
+        char[] charArray = src.toCharArray();
+        int length = charArray.length;
+        char[] reversedArray = new char[length];
+
+        for (int i = 0; i < length; i++) {
+            reversedArray[length - 1 - i] = charArray[i];
         }
-        return reversed.toString();
+
+        return new String(reversedArray);
     }
 
     public static String reverse(String src, String dest) {
@@ -20,23 +24,41 @@ public class Main {
         if (index == -1) {
             return src;
         }
-        StringBuilder reversed = new StringBuilder();
-        for (int i = dest.length() - 1; i >= 0; i--) {
-            reversed.append(dest.charAt(i));
+        char[] srcArray = src.toCharArray();
+        char[] destArray = dest.toCharArray();
+        int destLength = destArray.length;
+        char[] reversedDestArray = new char[destLength];
+
+        for (int i = 0; i < destLength; i++) {
+            reversedDestArray[destLength - 1 - i] = destArray[i];
         }
-        return src.substring(0, index) + reversed.toString() + src.substring(index + dest.length());
+
+        String result = "";
+        for (int i = 0; i < srcArray.length; i++) {
+            if (i == index) {
+                result += new String(reversedDestArray);
+                i += destLength - 1;
+            } else {
+                result += srcArray[i];
+            }
+        }
+
+        return result;
     }
 
     public static String reverse(String src, int firstIndex, int lastIndex) {
         if (firstIndex < 0 || lastIndex >= src.length() || firstIndex > lastIndex) {
             return src;
         }
-        StringBuilder reversed = new StringBuilder();
-        for (int i = lastIndex; i >= firstIndex; i--) {
-            reversed.append(src.charAt(i));
+        char[] srcArray = src.toCharArray();
+
+        for (int i = firstIndex; i <= (firstIndex + lastIndex) / 2; i++) {
+            char temp = srcArray[i];
+            srcArray[i] = srcArray[lastIndex - i + firstIndex];
+            srcArray[lastIndex - i + firstIndex] = temp;
         }
-        return src.substring(0, firstIndex) + reversed.toString() + src.substring(lastIndex + 1);
+
+        return new String(srcArray);
     }
 
 }
-
