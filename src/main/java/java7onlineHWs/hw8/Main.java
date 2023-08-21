9b6956feecd3456bbc36f3851c1f2c47b019ed99
TimurBondarenko <timurbondarenko404@gmail.com>
2023-08-21 13:26:19 +0300

    Дз 8

--- src/main/java/java7onlineHWs/hw8/Main.java
+++ src/main/java/java7onlineHWs/hw8/Main.java
@@ -1,4 +1,4 @@
-package ua.com.alevel;
+package java7onlineHWs.hw8;
 
 public class Main {
 
@@ -8,7 +8,11 @@ public class Main {
     }
 
     public static String reverse(String src) {
-        return new StringBuilder(src).reverse().toString();
+        StringBuilder reversed = new StringBuilder();
+        for (int i = src.length() - 1; i >= 0; i--) {
+            reversed.append(src.charAt(i));
+        }
+        return reversed.toString();
     }
 
     public static String reverse(String src, String dest) {
@@ -16,14 +20,23 @@ public class Main {
         if (index == -1) {
             return src;
         }
-        return new StringBuilder(src).replace(index, index + dest.length(), new StringBuilder(dest).reverse().toString()).toString();
+        StringBuilder reversed = new StringBuilder();
+        for (int i = dest.length() - 1; i >= 0; i--) {
+            reversed.append(dest.charAt(i));
+        }
+        return src.substring(0, index) + reversed.toString() + src.substring(index + dest.length());
     }
 
     public static String reverse(String src, int firstIndex, int lastIndex) {
         if (firstIndex < 0 || lastIndex >= src.length() || firstIndex > lastIndex) {
             return src;
         }
-        return new StringBuilder(src).replace(firstIndex, lastIndex + 1, new StringBuilder(src.substring(firstIndex, lastIndex + 1)).reverse().toString()).toString();
+        StringBuilder reversed = new StringBuilder();
+        for (int i = lastIndex; i >= firstIndex; i--) {
+            reversed.append(src.charAt(i));
+        }
+        return src.substring(0, firstIndex) + reversed.toString() + src.substring(lastIndex + 1);
     }
+
 }
 
